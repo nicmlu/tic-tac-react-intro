cd7d71c3d144864ad6a2fb5e3eba746a297de103
nicmlu <NicoleMLucien@gmail.com>
2021-01-23 22:45:08 -0500

    created components and props

--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import "./index.css";
 class Square extends React.Component {
   render() {
     return (
-      <button
-        className="square"
-        onClick={function() {
-          alert("click");
-        }}
-      >
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
@@ -18,8 +13,17 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { squares: Array(9).fill(null) };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
